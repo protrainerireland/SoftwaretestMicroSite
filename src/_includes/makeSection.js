f2efcc7504fe9b78e8250442bd7c6ca6021f5ef8
CALUMONEILL <120475986@umail.ucc.ie>
2022-05-16 16:27:30 +0100

    Commit before pulling from master

--- src/_includes/makeSection.js
+++ src/_includes/makeSection.js
@@ -189,8 +189,7 @@ module.exports = {
             case "keywordtext":
 
 
-                //let defaultTemplate = `<div>${site.searchKeywords.join(" ")}</div>`;
-                let defaultTemplate = `${site.searchKeywords[0]} is one of the most popular coding languages in the world. The job market for ${ site.searchKeywords[0] } developers is robust and consistent, making it a great programming language to learn. Our courses can give you the skills you need to work effectively with ${ site.searchKeywords[0] }.  Why attend one of our ${ site.searchKeywords[0] } courses? If you're interested working effectively with ${ site.searchKeywords[0] } to benefit your organisation and to get an edge over your competitors, or simply to learn a highly lucrative skill then you should consider our ${ site.searchKeywords[0] } training course.</p>`
+                
                 
                 if (section.image) {
                     //..tbd
@@ -208,7 +207,8 @@ module.exports = {
                         data-aos-anchor="#${section.id}"
                         >
                         <p class="about-title">${ section.content.title }</p>
-                        <p>${ defaultTemplate }</p>
+                        <p>${ site.content.text1 }</p>
+                        <p>${ site.content.text2 }</p>
                         </div>`;
                     if (section.image) {
                         html += `<div class="col-md-6" 
@@ -322,6 +322,38 @@ module.exports = {
                             </div>
                         </section>`;
                 break;
+
+                case "summary":
+
+                    list = `<ul>${section.content.list.map(item=>`<li>${item}</li>`).join("")}</ul>`;
+                    
+                    html = `<section id="${section.id}" class="section">
+                        <div class="container">
+                        <h3 class="title text-center">${ section.title }</h3>
+                        <div class="row ${section.imagePosition=="left" ? "flex-row-reverse" :""}">
+                            <div class="${section.image ? 'col-md-6' : 'col-md-12'} about"
+                            data-aos="fade-left" ${ animationDelay } ${animationOffset}
+                            data-aos-anchor="#${section.id}"
+                            >
+                            <p class="about-title">${ section.content.title }</p>
+                            <p>${ section.content.text1 }</p>
+                            <p>${ section.content.text2 }</p>
+                            ${list}
+                            <p>${ section.content.text3 }</p>
+                            </div>`;
+                        if (section.image) {
+                            html += `<div class="col-md-6"
+                                    data-aos="fade-right" ${ animationDelay }  ${animationOffset}
+                                    data-aos-anchor="#${section.id}"
+                            >
+                                        <img src="${ section.image }" class="img-fluid" alt="">
+                                    </div>`;
+                        }    
+        
+                    html += `</div>
+                        </div>
+                    </section>`;
+                    break;
 
             case 'pricing':
 
